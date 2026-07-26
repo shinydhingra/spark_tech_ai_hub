e0a1bfe2f74483c7cf44b8c0cf13c9934041a482
shinydhingra <shinydhingra2006@gmail.com>
2026-07-26 08:48:47 +0530

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+import tsConfigPaths from "vite-tsconfig-paths";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [
+    tanstackStart(),
+    react(),
+    tailwindcss(),
+    tsConfigPaths(),
+  ],
+});
